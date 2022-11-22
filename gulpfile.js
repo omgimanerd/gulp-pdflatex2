5d26bc1eba450cfd47c1e034c570abf582b27db9
Alvin Lin <alvin@omgimanerd.tech>
2022-11-22 02:24:13 -0500

    Updated test gulpfile and gulp versions

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,23 @@
 
 const gulp = require('gulp')
 const rename = require('gulp-rename')
-const pdflatex = require('./gulp-pdflatex2')
 
-gulp.task('default', ['test'])
+const pdflatex = require('./gulp-pdflatex2.js')
 
-gulp.task('test', () => {
+console.log(pdflatex)
+
+const test = () => {
   return gulp.src('./tests/*.tex')
     .pipe(pdflatex({
       cliOptions: ['-shell-escape'],
       keepIntermediateFiles: './tmp',
-      texInputs: ['./tests/cls']
+      texInputs: ['./tests/cls'],
+      separator: process.platform === 'win32' ? ';' : ':'
     }))
     .pipe(rename(path => {
       path.dirname += '/output'
     }))
-    .pipe(gulp.dest('./tests'))
-})
+    .pipe(gulp.dest('./tests'));
+};
+
+exports.default = test
